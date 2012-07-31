4bd84bbb7be9c76f57e831969d6b4c0af6bf295f
SP <saurabhp@synechron.com>
2012-07-31 15:48:09 -0400

    adding rspec for validations and associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+	  before(:each){User.create!(first_name:'abc',last_name: 'xyz',email: 'abc@xyz.com',username: 'abcxyz')}
+	  it{should validate_presence_of(:email)}
+	  it{should validate_presence_of(:username)}
+	  it{should validate_format_of(:email)}
+	  it{should validate_format_of(:username)}
+  end
+
+  context "Associations" do
+	  it { should have_many :user_roles }
+	  it { should have_many :roles }
+  end
 end
