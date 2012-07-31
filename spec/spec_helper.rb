f04bfadbd167fe56dbd3589d2d0731bad4e4bad2
SP <saurabhp@synechron.com>
2012-07-31 13:21:49 -0400

    adding options to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,18 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+#require Factory Girl to create factories
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.color_enabled = true
+  config.tty = true
+  config.formatter = :documentation
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
